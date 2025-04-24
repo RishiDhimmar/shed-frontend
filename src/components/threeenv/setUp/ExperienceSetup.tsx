704b11d78d71dd40d2f559fe76c3e89e4496b7e5
RishiDhimmar <rishi@hexacoder.com>
2025-04-24 15:13:20 +0530

    "done"

--- src/components/threeenv/setUp/ExperienceSetup.tsx
+++ src/components/threeenv/setUp/ExperienceSetup.tsx
@@ -1,15 +1,12 @@
 import CameraSetup from "./CameraSetup";
 import ControlsSetup from "./ControlsSetup";
-import LightSetup from "./LightSetup";
 import ShadeVisualizer from "./ShadeVisualizer";
 
 function ExperienceSetup() {
   return (
     <>
-      <LightSetup />
       <ControlsSetup />
       <CameraSetup />
-
       <ShadeVisualizer />
     </>
   );
